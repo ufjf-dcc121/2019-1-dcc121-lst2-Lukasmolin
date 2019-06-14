52ea8eb0adc9ce6f9a46423a6b8ecc4fb9868104
Lucas Molin <38398230+Lukasmolin@users.noreply.github.com>
2019-06-14 11:35:07 -0300

    Adiciona funcionalidade Excluir
    Cria a funcionalidade Excluir e faz pequenas correções

--- qst03.js
+++ qst03.js
@@ -2,8 +2,8 @@ document.addEventListener('DOMContentLoaded', iniciar)
 var f, la, lm
 function iniciar(){
     f = new Formulario()
-    la = new Lista(document.getElementById('listaAdultos'), 'teste')
-    lm = new Lista(document.getElementById('listaMenores'), 'teste')
+    la = new Lista(document.getElementById('listaAdultos'), f)
+    lm = new Lista(document.getElementById('listaMenores'), f)
     configura(f, la, lm)
 }
 
@@ -18,19 +18,38 @@ function Formulario() {
     this._formElemento = document.forms.formInscricao
 }
 
+Formulario.prototype.gerarBotaoExcluir = function(liElemento){
+    if(this._formElemento.lastElementChild.type == 'button'){
+        this._formElemento.lastElementChild.remove()
+    }    
+    var btn = document.createElement('input')
+    btn.type = 'button'
+    btn.value = 'Excluir'
+    btn.addEventListener('click', function(){
+        liElemento.remove()
+        btn.remove()
+    })
+    this._formElemento.appendChild(btn)
+}
+
 Formulario.prototype.getPessoa = function () {
     return new Pessoa(this._formElemento.inputNome.value, this._formElemento.inputIdade.value)
 }
 
 /*Classe Lista*/
-function Lista(elemento){
+function Lista(elemento, form){
     this._olElemento = elemento
+    this._form = form
 }
 
 Lista.prototype.adicionar = function(pessoa){
     var li = document.createElement('li')
     li.textContent = pessoa.nome + " - " + pessoa.idade
     li.contentEditable = true
+    var self = this
+    li.addEventListener('focus', function(e){
+        self._form.gerarBotaoExcluir(li)
+    })
     this._olElemento.appendChild(li)
 }
 
@@ -44,5 +63,8 @@ function configura(formulario, listaAdultos, listaMenores){
         } else {
             listaAdultos.adicionar(pessoa)
         }
+        formulario._formElemento.inputNome.value = ""
+        formulario._formElemento.inputIdade.value = ""
+        formulario._formElemento.inputNome.focus()
     })
 }
