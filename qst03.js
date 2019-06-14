ab42901685e06450adff995de676a90042367ae5
Lucas Molin <38398230+Lukasmolin@users.noreply.github.com>
2019-06-14 10:06:02 -0300

    Cria classe Formulario e corrige Pessoa
    Cria e implementa a classe responsavel por manipular o formulário
    
    Melhora classe Pessoa

--- qst03.js
+++ qst03.js
@@ -1,11 +1,30 @@
 document.addEventListener('DOMContentLoaded', iniciar)
-
+var f
 function iniciar(){
-
+    f = new Formulario(/*manipulador*/)
 }
 
 /*Classe Pessoa*/
 function Pessoa(nome, idade) {
     this.nome = nome
-    this.idade = idade
+    this.idade = parseInt(idade)
+}
+
+/*Classe Formulario*/
+function Formulario(manipulador) {
+    this._manipulador = manipulador
+    this._formElemento = document.forms.formInscricao
+    this._inicializar()
+}
+
+Formulario.prototype._inicializar = function () {
+    var self = this
+    this._formElemento.addEventListener('submit', function(e){
+        e.preventDefault()
+        console.log(self.getPessoa())//Temp
+    })
+}
+
+Formulario.prototype.getPessoa = function () {
+    return new Pessoa(this._formElemento.inputNome.value, this._formElemento.inputIdade.value)
 }
