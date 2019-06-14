b51c82bd5692d4c17af23c3921c9f87f2bef9a16
Lucas Molin <38398230+Lukasmolin@users.noreply.github.com>
2019-06-14 11:08:08 -0300

    Cria elemento e classe Lista e correções
    Corrige classe formulário excluindo metodos desnecessários
    
    Cria Classe Lista para manipular as Listas no documento

--- qst03.js
+++ qst03.js
@@ -1,7 +1,10 @@
 document.addEventListener('DOMContentLoaded', iniciar)
-var f
+var f, la, lm
 function iniciar(){
-    f = new Formulario(/*manipulador*/)
+    f = new Formulario()
+    la = new Lista(document.getElementById('listaAdultos'), 'teste')
+    lm = new Lista(document.getElementById('listaMenores'), 'teste')
+    configura(f, la, lm)
 }
 
 /*Classe Pessoa*/
@@ -11,20 +14,35 @@ function Pessoa(nome, idade) {
 }
 
 /*Classe Formulario*/
-function Formulario(manipulador) {
-    this._manipulador = manipulador
+function Formulario() {
     this._formElemento = document.forms.formInscricao
-    this._inicializar()
-}
-
-Formulario.prototype._inicializar = function () {
-    var self = this
-    this._formElemento.addEventListener('submit', function(e){
-        e.preventDefault()
-        console.log(self.getPessoa())//Temp
-    })
 }
 
 Formulario.prototype.getPessoa = function () {
     return new Pessoa(this._formElemento.inputNome.value, this._formElemento.inputIdade.value)
+}
+
+/*Classe Lista*/
+function Lista(elemento){
+    this._olElemento = elemento
+}
+
+Lista.prototype.adicionar = function(pessoa){
+    var li = document.createElement('li')
+    li.textContent = pessoa.nome + " - " + pessoa.idade
+    li.contentEditable = true
+    this._olElemento.appendChild(li)
+}
+
+/*Função Configura*/
+function configura(formulario, listaAdultos, listaMenores){
+    formulario._formElemento.addEventListener('submit', function(e) {
+        e.preventDefault()
+        var pessoa = formulario.getPessoa()
+        if(pessoa.idade < 18){
+            listaMenores.adicionar(pessoa)
+        } else {
+            listaAdultos.adicionar(pessoa)
+        }
+    })
 }
